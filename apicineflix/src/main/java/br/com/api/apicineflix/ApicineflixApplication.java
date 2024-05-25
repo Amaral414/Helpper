b9c33e674a6fef1ade944cc3fe59b5664b17732b
Gabriel Amaral <gabriel.amaral414@hotmail.com>
2024-05-25 02:21:23 -0300

    Initial commit
    novo

---
+++ apicineflix/src/main/java/br/com/api/apicineflix/ApicineflixApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.apicineflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicineflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicineflixApplication.class, args);
+	}
+
+}
---
+++ apicineflix/src/test/java/br/com/api/apicineflix/ApicineflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.apicineflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicineflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
