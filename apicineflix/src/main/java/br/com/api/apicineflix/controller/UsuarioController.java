1606694260aefd5cd8000d339248ad7ac3f381c8
Gabriel Amaral <gabriel.amaral414@hotmail.com>
2024-05-25 02:27:01 -0300

    Cadastro
    Criei o primeiro Endpoint, o de cadastro

---
+++ apicineflix/src/main/java/br/com/api/apicineflix/controller/UsuarioController.java
@@ -0,0 +1,14 @@
+package br.com.api.apicineflix.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+
+    @GetMapping("/cadastro")
+    public String texto(){
+        return "Acessando a API com sucesso";
+
+    }
+}
