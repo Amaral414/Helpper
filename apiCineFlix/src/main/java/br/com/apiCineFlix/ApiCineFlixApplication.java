a3230e7837172d47576b6eaa2d13e9513e3c88eb
Gabriel Amaral <gabriel.amaral414@hotmail.com>
2024-05-25 01:39:27 -0300

    Primeiro Commit
    Configurações iniciais da API

---
+++ apiCineFlix/src/main/java/br/com/apiCineFlix/ApiCineFlixApplication.java
@@ -0,0 +1,13 @@
+package br.com.apiCineFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCineFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCineFlixApplication.class, args);
+	}
+
+}
---
+++ apiCineFlix/src/test/java/br/com/apiCineFlix/ApiCineFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apiCineFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCineFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
