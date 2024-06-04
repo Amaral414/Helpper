ea5dd51e2b3191965c313ae368f7ba3369965c79
Gabriel Amaral <gabriel.amaral414@hotmail.com>
2024-06-04 12:07:38 -0300

    Update UsuarioController.java

--- apicineflix/src/main/java/br/com/api/apicineflix/controller/UsuarioController.java
+++ apicineflix/src/main/java/br/com/api/apicineflix/controller/UsuarioController.java
@@ -1,8 +1,11 @@
 package br.com.api.apicineflix.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +14,17 @@ import br.com.api.apicineflix.DAO.IUsuario;
 import br.com.api.apicineflix.model.Usuario;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
+/*
+ Quando eu coloco a URL (http://localhost:8080[/path])
+ vai sempre chamar o método que está indicado depois do Mapping
+ */
 @RestController
-@RequestMapping("/usuarios") // Mapa de solicitação
+@CrossOrigin("*")
+@RequestMapping("/usuarios") //Mapa de solicitação
 public class UsuarioController {
     /*
     A cada método que eu for chamar (Post, Get, Delete)
@@ -25,17 +35,31 @@ public class UsuarioController {
     @Autowired
     private IUsuario dao;
  
-    @GetMapping
+    @GetMapping // método Get
     public List<Usuario> listaUsuarios(){
         return (List<Usuario>) dao.findAll();
         
         // Até aqui tudo tranquilo
     }
 
-    @PostMapping
+    @PostMapping // método Post
     public Usuario postMethodName(@RequestBody Usuario usuario) {
         Usuario usuarioNovo = dao.save(usuario);
         return usuarioNovo;
+    }
+
+    @PutMapping // método Put
+    public Usuario editarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        
+        return usuarioNovo;
+    }
+
+    @DeleteMapping("/{id}") // método Delete
+    public Optional<Usuario> excluirUsuario(@PathVariable int id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
     }
     
 }
